2a37c1efb05762400323aeb637f96d7cd42759e7
woodelin-florveus <woodelinflorveus@gmail.com>
2021-10-19 14:59:48 -0400

    added the require for loading

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
 module MyBowlBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
