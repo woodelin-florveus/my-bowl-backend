0efb382a531a57299e5bdba1f022ef064fae45ff
woodelin-florveus <woodelinflorveus@gmail.com>
2021-10-28 11:12:46 -0400

    updated keys and id

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,15 +22,17 @@ MealPlan.reset_pk_sequence
 # response = RestClient.get('https://api.edamam.com/api/recipes/v2?type=public&q=Rice&app_id=6090520d&app_key=' + ENV["RECIPE_KEY"])
 # recipe_array = JSON.parse(response)
 
-response = RestClient.get('https://api.edamam.com/api/recipes/v2?type=public&q=Rice&app_id=6090520d&app_key=' + ENV["RECIPE_KEY"])
-recipe_array = JSON.parse(response)
+# response = RestClient.get('https://api.edamam.com/api/recipes/v2?type=public&q=Rice&app_id=6090520d&app_key=' + ENV["RECIPE_KEY"])
 
 
- user_1 = User.create(name:"Woodelin", email:"woodelinflorveus@hotmail.com", bio:"love to plan out my meal", image:"https://img1.10bestmedia.com/Images/Photos/376708/GettyImages-1140201336_54_990x660.jpg")
+byebug
 
- meal_1 = Meal.create(name:"pasta", image:"https://www.edamam.com/web-img/bb5/bb5bad0cbcb94ad2ef0895d444f30291.jpg", calories: 200, nutrition_info:"Balanced")
 
- meal_plan_1 = MealPlan.create(title:"Morning Breakfast", image:"shorturl.at/evEO8", user_id: user_1.id, meal_id: meal_1.id)
+#  user_1 = User.create(name:"Woodelin", email:"woodelinflorveus@hotmail.com", bio:"love to plan out my meal", image:"https://img1.10bestmedia.com/Images/Photos/376708/GettyImages-1140201336_54_990x660.jpg")
+
+#  meal_1 = Meal.create(name:"pasta", image:"https://www.edamam.com/web-img/bb5/bb5bad0cbcb94ad2ef0895d444f30291.jpg", calories: 200, nutrition_info:"Balanced")
+
+#  meal_plan_1 = MealPlan.create(title:"Morning Breakfast", image:"shorturl.at/evEO8", user_id: user_1.id, meal_id: meal_1.id)
 
 
 
