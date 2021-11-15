7cb2cff921992bac84f8b407ec637ef1ae8fc957
woodelin-florveus <woodelinflorveus@gmail.com>
2021-11-14 19:43:57 -0500

    updated seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,14 @@ MealPlan.reset_pk_sequence
 
 
 # add user get more specific and edit along the way 
-Meal.get_data
+
+user1 = User.create(name:"woodelin", email: "woodelin@hotmail.com", bio:"love to try new things", image:"https://cdn.loveandlemons.com/wp-content/uploads/2018/09/IMG_12798-crop2-1.jpg")
+
+meal1 = Meal.create(name:"Rice", image:"https://media.newyorker.com/photos/5f2c85539a557880d973a759/1:1/w_1823,h_1823,c_limit/Buford-FrenchRice.jpg", calories:1000, nutrition_info:"good stuff")
+
+mealplan1 = MealPlan.create(title:"Breakfast", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Breakfast_Platter.jpg/1200px-Breakfast_Platter.jpg", user_id: user1.id, meal_id: meal1.id)
+
+# Meal.get_data
 # add joiner 
 
 
